6a700f51fe057005b8810b18e4fe8c56f765b796
shenkev <tingkekevin@hotmail.com>
2017-06-03 22:52:41 -0700

    Changed port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/lib'));
 
-var port = 5000;
+const port = (process.env.PORT || 8080)
 
 app.get('*', function (req, res) {
 	res.sendFile(__dirname + '/dist/index.html');
